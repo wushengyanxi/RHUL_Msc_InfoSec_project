0e377fda58ec04bc903b55997cae135adbd34c31
wushengyanxi <49746087+wushengyanxi@users.noreply.github.com>
2024-07-02 23:08:57 +0800

    general update
    I started to implement the server, for now, I add one function and one procedure inside, they will write training set in a txt file as the system log document. The other file will read data from the file since we need write user traffic in and re-training models by the content in this log document.
    
    There is a testcase in server testcase file, this testcase prove that the sample been read out will keep exactly same format before they written in

--- IDS_Structure/IDS_model_v1/server.py
+++ IDS_Structure/IDS_model_v1/server.py
@@ -1,12 +1,36 @@
 import time
 import sys
+import csv
+
 sys.path.append(r'C:\Users\wushe\Desktop\RHUL_Msc_InfoSec_project')
 sys.path.append(r'C:\Users\wushe\Desktop\RHUL_Msc_InfoSec_project\IDS_Structure')
 sys.path.append(r'C:\Users\wushe\Desktop\RHUL_Msc_InfoSec_project\DataSet_Preprocesser')
 from Training_Set_Creator import Training_set_create
-from Get_Training_Result import Ensemble_Lrarning_Training
+from Get_Training_Result import Ensemble_Learning_Training
 
-Features_List, Training_Data_Set, Testing_Data_Set = Training_set_create()
-Training_Result = Ensemble_Lrarning_Training(Features_List, Training_Data_Set)
+
+def initialize_log_file(features_list, training_data_set):
+    with open('IDS_traffic_log.txt', 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(features_list)
+        writer.writerows(training_data_set)
+
+
+def read_log_file():
+    def convert_to_original_type(value):
+        try:
+            return int(value)
+        except ValueError:
+            try:
+                return float(value)
+            except ValueError:
+                return value
+
+    with open('IDS_traffic_log.txt', 'r', newline='') as file:
+        reader = csv.reader(file)
+        new_features_list = next(reader)
+        new_training_data_set = [list(map(convert_to_original_type, row)) for row in reader]
+    return new_features_list, new_training_data_set
+
 
 
---
+++ IDS_Structure/IDS_model_v1/server_testcase.py
@@ -0,0 +1,42 @@
+import time
+import sys
+import csv
+
+sys.path.append(r'C:\Users\wushe\Desktop\RHUL_Msc_InfoSec_project')
+sys.path.append(r'C:\Users\wushe\Desktop\RHUL_Msc_InfoSec_project\IDS_Structure')
+sys.path.append(r'C:\Users\wushe\Desktop\RHUL_Msc_InfoSec_project\DataSet_Preprocesser')
+from Training_Set_Creator import Training_set_create
+from Get_Training_Result import Ensemble_Learning_Training
+from server import initialize_log_file
+from server import read_log_file
+
+'''
+
+
+This is a testcase for "initialize_log_file" and "read_log_file"
+
+We need make sure the feature list and the sample will keep exactly same when they were written in file and been read
+
+In testcase, when we get Features_List and Training_Data_Set by Training_set_create()
+
+we first store them in variables, for training set data, we choose the first and the 1314th to store
+
+and after new Features_List and Training_Data_Set been read from file, we check if the corresponding content is exactly the same
+--------------
+
+Features_List, Training_Data_Set, Testing_Data_Set = Training_set_create()
+
+test_feature = Features_List
+testcase_sample = Training_Data_Set[0]
+testcase_sample2 = Training_Data_Set[1314]
+
+initialize_log_file(Features_List, Training_Data_Set)
+
+Features_List, Training_Data_Set = read_log_file()
+
+if Features_List == test_feature and testcase_sample == Training_Data_Set[0] and testcase_sample2 == Training_Data_Set[1314]:
+    print("pass test")
+else:
+    print("not pass")
+
+'''
